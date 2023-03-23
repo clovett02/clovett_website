dcdc092a73ff11f8998fd840bfc3cdc800be3d42
Chris Lovett <chris.lovett02@yahoo.net>
2023-03-23 03:56:59 -0400

    Updated and refactored css files. Included more accurate padding and postioning tailored to each page and unreliant on the main.css file.

--- website/static/javascript/sidepanel.js
+++ website/static/javascript/sidepanel.js
@@ -1,6 +1,6 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "200px";
+    document.getElementById("mySidepanel").style.width = "170px";
   }
   
   /* Set the width of the sidebar to 0 (hide it) */
