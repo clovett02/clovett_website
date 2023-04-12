15086aac51cb2bf75700c3f61dbe00fc3d971a14
Chris Lovett <chris.lovett02@yahoo.net>
2023-04-12 15:39:10 -0400

    _

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from website import create_app
 from flask import request, redirect
-from flask_talisman import Talisman
 
 if __name__ == "__main__":
     app = create_app()
-    Talisman(app)
 
     app.run(host="0.0.0.0", port=5000)
