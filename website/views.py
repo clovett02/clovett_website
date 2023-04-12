5704a528107f665fe26f501a042ba91859dbb72e
Chris Lovett <chris.lovett02@yahoo.net>
2023-04-12 13:32:07 -0400

    added a redirect

--- website/views.py
+++ website/views.py
@@ -1,6 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 
 views = Blueprint('views', __name__)
+
+@views.before_request
+def before_request():
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
 
 @views.route('/')
 def Home():
