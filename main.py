9a86dce38214728ea93bfc2d1822c7044dcad70f
Chris Lovett <chris.lovett02@yahoo.net>
2023-04-12 13:36:24 -0400

    testing change

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from website import create_app
+from flask import request, redirect
 
 if __name__ == "__main__":
     app = create_app()
+
+    @app.before_request
+    def before_request():
+        if not request.is_secure:
+            url = request.url.replace('http://', 'https://', 1)
+            code = 301
+            return redirect(url, code=code)
 
     app.run(host="0.0.0.0", port=80)
